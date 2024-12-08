a28b3a35a1c49e6c86d350234c7383e03fe52d60
Utkarsh Pant <utkarshpant1994@gmail.com>
2024-12-08 10:31:49 +0530

    Week 12.4 Joins in SQL

--- week 12/12.4 SQL and Joins/src/index.ts
+++ week 12/12.4 SQL and Joins/src/index.ts
@@ -47,6 +47,78 @@ async function insertIntoTable(
   console.log(result);
 }
 
+// Transactions
+
+type UserAddress = {
+  username: string;
+  email: string;
+  password: string;
+  street: string;
+  city: string;
+  country: string;
+};
+
+async function insertUserAndAddress(userAddress: UserAddress) {
+  try {
+    await client.connect();
+
+    // Start Transction
+    await client.query("BEGIN");
+
+    const { username, email, password, street, city, country } = userAddress;
+
+    // Insert into users table
+
+    const insertUserQuery = `
+  INSERT INTO users (username, email, password)
+  VALUES ($1, $2, $3);
+  `;
+    const usersRes = await client.query(insertUserQuery, [
+      username,
+      email,
+      password,
+    ]);
+
+    // const userId = usersRes.rows[0].id;
+
+    console.log("User result ", usersRes);
+
+    const insertAddQuery = `
+  INSERT INTO addresses (user_id, street, city, country)
+  VALUES($1, $2, $3);
+  `;
+
+    const addResult = await client.query(insertAddQuery, [
+      // userId,
+      street,
+      city,
+      country,
+    ]);
+
+    console.log("Add result ", addResult);
+
+    // Commit transaction
+    await client.query("COMMIT");
+
+    console.log("User and address inserted successfully");
+  } catch (err) {
+    await client.query("ROLLBACK"); // Roll back the transaction on error
+    console.error("Error during transaction, rolled back.", err);
+    throw err;
+  } finally {
+    await client.end(); // Close the client connection
+  }
+}
+
+insertUserAndAddress({
+  username: "tanmay_bhatt",
+  email: "tanmay@gmail.com",
+  password: "tanmay@123",
+  street: "Bandra",
+  city: "Mumbai",
+  country: "India",
+});
+
 // createUsersTable();
 
 // insertIntoTable("roger_federer", "roger@gmail.com", "@123Roger");
