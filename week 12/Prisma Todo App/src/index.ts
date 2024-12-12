9d496323443231c68f0205759fe79305fbda599c
Utkarsh Pant <utkarshpant1994@gmail.com>
2024-12-12 22:12:23 +0530

    Added exception handling in signup handler

--- week 12/Prisma Todo App/src/index.ts
+++ week 12/Prisma Todo App/src/index.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
+import { createUser } from "./db/user";
 
 //Configuring Dotenv to use environment variables from .env file
 config();
@@ -12,6 +13,24 @@ app.use(express.json());
 
 app.get("/", (req: any, res: any) => {
   res.send("Hello World");
+});
+
+app.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const response = await createUser(username, email, password.toString());
+
+    console.log("User created ", response);
+
+    res.json({
+      message: "User created successfully with id ",
+    });
+  } catch (err) {
+    res.status(401).json({
+      msg: "Email/ Username already exists",
+    });
+  }
 });
 
 app.listen(port, () => {
