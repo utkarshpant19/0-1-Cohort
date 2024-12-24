e344f3095599aac219a562baf818f24b1e0927a9
Utkarsh Pant <utkarshpant1994@gmail.com>
2024-12-25 01:25:40 +0530

    Prisma Connection pool configuration

---
+++ week 12/12.6 Cloudflare Connection Pool/app/src/index.ts
@@ -0,0 +1,36 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+
+export interface Env {
+	DATABASE_URL: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const prisma = new PrismaClient({
+			datasourceUrl: env.DATABASE_URL,
+		}).$extends(withAccelerate());
+
+		prisma.user.create({
+			data: {
+				email: 'utkarsh@gmail.com',
+				name: 'Utkarsh Pant',
+			},
+		});
+
+		return new Response(`Request Method ${request.method}`);
+	},
+} satisfies ExportedHandler<Env>;
